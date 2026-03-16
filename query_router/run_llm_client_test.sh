897f4bf7b5b1c828461bdfb5f688ebaf01ca3fb2
Evgeny Koshelev <eko2000@gmail.com>
2026-03-16 18:43:50 +0100

    Update test user requests

--- query_router/run_llm_client_test.sh
+++ query_router/run_llm_client_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python test_llm_client.py \
-    "Please find a flight and a hotel for a trip to London from Zurich from 15.03.2026 to 21.03.2026.
+    "Please find a flight and a hotel for a trip to London from Zurich from 15.05.2026 to 21.05.2026.
     The flight cost should be max. 200 GBP per traveller for the whole trip, the hotel cost is max. 300 GBP per day per person.
     The flight should be economy class.
     We would like to find a hotel close to the shard skyscraper.
@@ -14,8 +14,11 @@ python test_llm_client.py \
     There will be three travelers on this trip. Every traveller should stay in a single room."
 
 python test_llm_client.py \
-    "Please plan my trip from Zurich at 12.03.2026 to Beijing, staying there for three days,
+    "Please plan my trip from Zurich at 01.05.2026 to Beijing, staying there for three days,
     then flight to Hong Kong, staying there for two days,
     then to Singapore with 2 days staying,
-    then to New Delhi with 5 days staying, then back to Zurich.
+    then to New Delhi with 5 days staying,
+    then to Istanbul with 4 days staying,
+    then to Rio de Janeiro with 3 days staying,
+    then back to Zurich.
     The total flight budget is 3000 CHF, the hotel budget is 300 CHF per day per person."
